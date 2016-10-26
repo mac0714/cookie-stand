162ecae81c03963637f96d106365fc2180a83e3f
mac <cleelaphisut@gmail.com>
2016-10-26 09:44:11 -0700

    Inprocess day 2

--- app.js
+++ app.js
@@ -1,140 +1,90 @@
-function getDailySalesProjections(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+'use strict';
+
+var time = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function Store(storeName, minCustomer, maxCustomer, avgCookiesSale) {
+  console.log('Store', storeName, minCustomer, maxCustomer, avgCookiesSale);
+  this.name = storeName;
+  this.minCust = minCustomer;
+  this.maxCust = maxCustomer;
+  this.cookies = avgCookiesSale;
+  this.daily = [];
+  this.total = 0;
+  this.randomNumCust = function() {
+    console.log('randomNumCust');
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  };
+  this.renderHTMLSales = function(){
+    console.log('renderHTMLSales');
+    //  Calculate all the cookie sales
+    this.calcCookieSales();
+    var h2 = document.createElement('h2');
+    h2.innerText = this.name;
+    var main = document.getElementsByTagName('main')[0];
+    main.appendChild(h2);
+
+    // Attaching list items with cookie sales per hour
+    var ul = document.createElement('ul');
+    for (var i = 0; i < time.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = time[i] + ': ' + this.daily[i] + ' cookies';
+      ul.appendChild(li);
+    }
+    var li = document.createElement('li');
+    li.innerText = this.total;
+    ul.appendChild(li);
+    // Append ul to the DOM
+    main.appendChild(ul);
+  };
+  this.calcCookieSales = function(){
+    console.log('calcCookieSales');
+    for (var i = 0; i < time.length; i++) {
+      var hourlyTotal = Math.floor(this.randomNumCust() * this.cookies);
+      this.total = hourlyTotal + this.total;
+      this.daily.push(hourlyTotal);
+    }
+
+    // Calculating and attaching total cookie sales
+    // var totalSales = 0;
+    // for (var i = 0; i < this.daily.length; i++){
+    //   totalSales += this.daily[i];
+    // }
+  };
 };
 
-var time = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var h1 = document.createElement('h1');
+h1.innerText = 'All Cookie Stores';
+var main = document.getElementsByTagName('main')[0];
+main.appendChild(h1);
 
-var store1 = {
-  name: '1st and Pike',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookies: 6.3,
-  dailySales: [], //Cal avg sales per day.
-  dailyCustomers: [], //Cal avg Customers per day.
-  calculateNumberCustomers: function() {
-    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer; //Avg custumer per hour.
-  },
-  calculateHourlySales: function(customer) {
-    return customer * this.avgCookies; //Avg sale per hour.
-  },
-  calculateDailySale: function() {
-    for(var i = 0; i < time.length; i++) {
-      var customerThisHours = this.calculateNumberCustomers();
-      console.log(time[i], customerThisHours);
-      this.dailyCustomers.push(customerThisHours);
-      var salesThisHours = this.calculateHourlySales(customerThisHours);
-      console.log(salesThisHours);
-      this.dailySales.push(salesThisHours);
-    }
-    console.log(this.dailyCustomers);
-    console.log(this.dailySales);
-  }
-};
-var store2 = {
-  name: 'SeaTac Airport',
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookies: 1.2,
-  dailySales: [], //
-  dailyCustomers: [], //
-  calculateNumberCustomers: function() {
-    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-  },
-  calculateHourlySales: function(customer) {
-    return customer * this.avgCookies;
-  },
-  calculateDailySale: function() {
-    for(var i = 0; i < time.length; i++) {
-      var customerThisHours = this.calculateNumberCustomers();
-      console.log(time[i], customerThisHours);
-      this.dailyCustomers.push(customerThisHours);
-      var salesThisHours = this.calculateHourlySales(customerThisHours);
-      console.log(salesThisHours);
-      this.dailySales.push(salesThisHours);
-    }
-    console.log(this.dailyCustomers);
-    console.log(this.dailySales);
-  }
-};
-var store3 = {
-  name: 'Seattle Center',
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookies: 3.7,
-  dailySales: [], //
-  dailyCustomers: [], //
-  calculateNumberCustomers: function() {
-    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-  },
-  calculateHourlySales: function(customer) {
-    return customer * this.avgCookies;
-  },
-  calculateDailySale: function() {
-    for(var i = 0; i < time.length; i++) {
-      var customerThisHours = this.calculateNumberCustomers();
-      console.log(time[i], customerThisHours);
-      this.dailyCustomers.push(customerThisHours);
-      var salesThisHours = this.calculateHourlySales(customerThisHours);
-      console.log(salesThisHours);
-      this.dailySales.push(salesThisHours);
-    }
-    console.log(this.dailyCustomers);
-    console.log(this.dailySales);
-  }
-};
+var store1 = new Store('1st and Pike', 23, 65, 6.3);
+var store2 = new Store('SeaTac Airport', 3, 24, 1.2);
+var store3 = new Store('Seattle Center', 11, 38, 3.7);
+var store4 = new Store('Capital Hill', 20, 38, 2.3);
+var store5 = new Store('Alki', 2, 16, 4.6);
 
-var store4 = {
-  name: 'Capital Hill',
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookies: 2.3,
-  dailySales: [], //
-  dailyCustomers: [], //
-  calculateNumberCustomers: function() {
-    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-  },
-  calculateHourlySales: function(customer) {
-    return customer * this.avgCookies;
-  },
-  calculateDailySale: function() {
-    for(var i = 0; i < time.length; i++) {
-      var customerThisHours = this.calculateNumberCustomers();
-      console.log(time[i], customerThisHours);
-      this.dailyCustomers.push(customerThisHours);
-      var salesThisHours = this.calculateHourlySales(customerThisHours);
-      console.log(salesThisHours);
-      this.dailySales.push(salesThisHours);
-    }
-    console.log(this.dailyCustomers);
-    console.log(this.dailySales);
-  }
-};
+store1.renderHTMLSales();
+store2.renderHTMLSales();
+store3.renderHTMLSales();
+store4.renderHTMLSales();
+store5.renderHTMLSales();
 
-var store5 = {
-  name: 'Seattle Center',
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookies: 4.6,
-  dailySales: [], //
-  dailyCustomers: [], //
-  calculateNumberCustomers: function() {
-    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
-  },
-  calculateHourlySales: function(customer) {
-    return customer * this.avgCookies;
-  },
-  calculateDailySale: function() {
-    for(var i = 0; i < time.length; i++) {
-      var customerThisHours = this.calculateNumberCustomers();
-      console.log(time[i], customerThisHours);
-      this.dailyCustomers.push(customerThisHours);
-      var salesThisHours = this.calculateHourlySales(customerThisHours);
-      console.log(salesThisHours);
-      this.dailySales.push(salesThisHours);
-    }
-    console.log(this.dailyCustomers);
-    console.log(this.dailySales);
-  }
-};
+var table = document.createElement('table');
+main.appendChild(table);
+var tr = document.createElement('tr');
+table.appendChild(tr);
+var td = document.createElement('td');
+for (var i = 0; i < time.length; i++) {
+  var td = document.createElement('td');
+  td.innerText = time[i];
+  tr.appendChild(td);
+}
+var storeLocation = ['1st and pike', 'SeaTac Airport', 'Seattle Center', 'Capital Hill', 'Alki'];
+document.write('<table>');
+document.write('<tr><th>Location</th></tr>');
+
+for(var i = 0; i < storeLocation.length; i++) {
+  document.write('<tr><td>' + storeLocation[i] + '</td></tr>');
+}
+
+document.while('</table>') ;
